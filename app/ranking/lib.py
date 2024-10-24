6af33e54a6129713be8b9a6e52609f8f06ce203d
colingdc <colin.grodecoeur@gmail.com>
2024-10-24 20:21:41 +0200

    Move weekly ranking to lib function
    # Conflicts:
    #	app/ranking/views.py

--- app/ranking/lib.py
+++ app/ranking/lib.py
@@ -1,6 +1,6 @@
 from sqlalchemy import func
 
-from ..models import Tournament
+from ..models import Tournament, Ranking
 
 
 def get_monthly_ranking(year, month):
@@ -23,3 +23,13 @@ def get_monthly_ranking(year, month):
             if participant.points is not None:
                 scores[user]["score"] += participant.points
     return sorted(scores.items(), key=lambda x: -x[1]["score"])
+
+
+def get_weekly_ranking(week=None):
+    if week is None:
+        week = Tournament.get_latest_finished_tournament().week
+    ranking = (Ranking.query
+               .filter(Ranking.tournament_week_id == week.id)
+               .order_by(Ranking.year_to_date_ranking)
+               )
+    return ranking
--- app/ranking/views.py
+++ app/ranking/views.py
@@ -5,9 +5,9 @@ from flask_babel import format_datetime
 
 from . import bp
 from ..decorators import login_required
-from ..models import Ranking, Tournament, TournamentWeek, TournamentStatus
+from ..models import Tournament, TournamentWeek, TournamentStatus
 from .forms import RankingForm, MonthlyRankingForm
-from .lib import get_monthly_ranking
+from .lib import get_monthly_ranking, get_weekly_ranking
 
 
 @bp.route("/<tournament_week_id>")
@@ -17,7 +17,7 @@ def ranking(tournament_week_id):
     if week.deleted_at:
         abort(404)
 
-    ranking = Ranking.get_ranking(week)
+    ranking = get_weekly_ranking(week)
 
     return render_template(
         "ranking/ranking.html",
