f3bbce9b04c263e4f759e4cea7bfe210f43c0c92
colingdc <colin.grodecoeur@gmail.com>
2024-10-24 21:29:22 +0200

    Delete useless commands file

--- manage.py
+++ manage.py
@@ -1,9 +1,3 @@
-from commands import (CloseTournament, CreateATPPlayers, CreateByePlayer,
-                      CreateFakePlayers, CreateFakeTournaments,
-                      CreateFakeUsers, CreateSpecialUsers,
-                      CreateTemporaryAccounts, CreateUsers, MergeAccounts,
-                      MergeAllAccounts, SendNewTournamentsEmail)
-
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager, Shell
 
@@ -35,20 +29,6 @@ migrate = Migrate(app, db)
 manager = Manager(app)
 manager.add_command("db", MigrateCommand)
 manager.add_command("shell", Shell(make_context=make_shell_context))
-
-
-manager.add_command("create_fake_users", CreateFakeUsers())
-manager.add_command("create_special_users", CreateSpecialUsers())
-manager.add_command("create_users", CreateUsers())
-manager.add_command("create_fake_tournaments", CreateFakeTournaments())
-manager.add_command("create_fake_players", CreateFakePlayers())
-manager.add_command("create_atp_players", CreateATPPlayers())
-manager.add_command("create_bye_player", CreateByePlayer())
-manager.add_command("create_temporary_accounts", CreateTemporaryAccounts())
-manager.add_command("close_tournament", CloseTournament())
-manager.add_command("merge_accounts", MergeAccounts())
-manager.add_command("merge_all_accounts", MergeAllAccounts())
-manager.add_command("send_new_tournaments_email", SendNewTournamentsEmail())
 
 
 if __name__ == "__main__":
