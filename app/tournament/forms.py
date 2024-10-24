968e1b6fbdff4bb20e50f1471ae625884789da33
colingdc <colin.grodecoeur@gmail.com>
2024-10-24 21:51:40 +0200

    Make forms indentation consistent

--- app/tournament/forms.py
+++ app/tournament/forms.py
@@ -4,35 +4,56 @@ from wtforms.validators import InputRequired, Optional
 
 
 class CreateTournamentForm(FlaskForm):
-    name = StringField('Nom',
-                       validators=[InputRequired(
-                           message="Ce champ est obligatoire")])
-    category = SelectField('Catégorie',
-                           validators=[InputRequired(
-                               message="Ce champ est obligatoire")])
-    start_date = DateTimeField('Date de début',
-                               format="%d/%m/%Y %H:%M",
-                               validators=[InputRequired(
-                                   message="Ce champ est obligatoire")])
-    week = DateTimeField('Semaine',
-                         format="%d/%m/%Y",
-                         validators=[InputRequired(
-                             message="Ce champ est obligatoire")])
+    name = StringField(
+        'Nom',
+        validators=[
+            InputRequired(message="Ce champ est obligatoire")
+        ]
+    )
+    category = SelectField(
+        'Catégorie',
+        validators=[
+            InputRequired(message="Ce champ est obligatoire")
+        ]
+    )
+    start_date = DateTimeField(
+        'Date de début',
+        format="%d/%m/%Y %H:%M",
+        validators=[
+            InputRequired(message="Ce champ est obligatoire")
+        ]
+    )
+    week = DateTimeField(
+        'Semaine',
+        format="%d/%m/%Y",
+        validators=[
+            InputRequired(message="Ce champ est obligatoire")
+        ]
+    )
     submit = SubmitField("Valider")
 
 
 class EditTournamentForm(FlaskForm):
-    name = StringField('Nom',
-                       validators=[InputRequired(
-                           message="Ce champ est obligatoire")])
-    start_date = DateTimeField('Date de début',
-                               format="%d/%m/%Y %H:%M",
-                               validators=[InputRequired(
-                                   message="Ce champ est obligatoire")])
-    week = DateTimeField('Semaine',
-                         format="%d/%m/%Y",
-                         validators=[InputRequired(
-                             message="Ce champ est obligatoire")])
+    name = StringField(
+        'Nom',
+        validators=[
+            InputRequired(message="Ce champ est obligatoire")
+        ]
+    )
+    start_date = DateTimeField(
+        'Date de début',
+        format="%d/%m/%Y %H:%M",
+        validators=[
+            InputRequired(message="Ce champ est obligatoire")
+        ]
+    )
+    week = DateTimeField(
+        'Semaine',
+        format="%d/%m/%Y",
+        validators=[
+            InputRequired(message="Ce champ est obligatoire")
+        ]
+    )
     submit = SubmitField("Valider")
 
 
