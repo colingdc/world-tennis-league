9e77f754192bdcb72ccc1c8ef0a73742b1f5cde1
colingdc <colin.grodecoeur@gmail.com>
2024-10-24 20:11:11 +0200

    Move monthly ranking to lib function

--- app/models.py
+++ app/models.py
@@ -723,24 +723,3 @@ class Ranking(db.Model):
                    .order_by(Ranking.year_to_date_ranking)
                    )
         return ranking
-
-    @staticmethod
-    def get_monthly_ranking(year, month):
-        tournaments = (Tournament
-                       .query
-                       .filter(func.year(Tournament.started_at) == year)
-                       .filter(func.month(Tournament.started_at) == month))
-        scores = {}
-
-        print(tournaments.count(), "tournaments")
-        for tournament in tournaments:
-            for participant in tournament.participations:
-                user = participant.user
-                if scores.get(user) is None:
-                    scores[user] = {
-                        "score": 0,
-                        "number_of_tournaments": 0
-                    }
-                scores[user]["score"] += participant.points
-                scores[user]["number_of_tournaments"] += 1
-        return sorted(scores.items(), key=lambda x: -x[1]["score"])
