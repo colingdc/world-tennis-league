3c06584d9cee6916b4433e940ffb1b95f8b358e2
colingdc <colin.grodecoeur@gmail.com>
2024-10-25 14:18:31 +0200

    Extract subfunctions in main app file

--- app/__init__.py
+++ app/__init__.py
@@ -89,6 +89,21 @@ def create_app(config_name):
     app.jinja_env.filters["dt"] = custom_datetime_filter
     app.jinja_env.filters["dt_diff"] = custom_datetime_difference_filter
 
+    register_blueprints(app)
+
+    from .errors import unauthorized, forbidden, page_not_found, bad_request, internal_server_error, unhandled_exception
+
+    app.register_error_handler(401, unauthorized)
+    app.register_error_handler(403, forbidden)
+    app.register_error_handler(404, page_not_found)
+    app.register_error_handler(400, bad_request)
+    # app.register_error_handler(500, internal_server_error)
+    # app.register_error_handler(Exception, unhandled_exception)
+
+    return app
+
+
+def register_blueprints(app):
     from .public import bp as public_blueprint
     app.register_blueprint(public_blueprint)
 
@@ -109,14 +124,3 @@ def create_app(config_name):
 
     from .player import bp as player_blueprint
     app.register_blueprint(player_blueprint, url_prefix="/wtl/player")
-
-    from .errors import unauthorized, forbidden, page_not_found, bad_request, internal_server_error, unhandled_exception
-
-    app.register_error_handler(401, unauthorized)
-    app.register_error_handler(403, forbidden)
-    app.register_error_handler(404, page_not_found)
-    app.register_error_handler(400, bad_request)
-    # app.register_error_handler(500, internal_server_error)
-    # app.register_error_handler(Exception, unhandled_exception)
-
-    return app
