38b7bd50b35bd198726315d40ce84332b66dc757
colingdc <colin.grodecoeur@gmail.com>
2024-11-05 13:56:44 +0100

    Extract dataTable config

---
+++ app/static/js/dataTableConfig.js
@@ -0,0 +1,17 @@
+dataTableConfig = {
+    language: {
+        search: "Rechercher",
+        lengthMenu: "Montrer _MENU_ lignes",
+        paginate: {
+            next: "Suivante",
+            previous: "Précédente"
+        }
+    },
+    dom: "ftlp",
+    searching: true,
+    paging: true,
+    pageLength: 100,
+    responsive: true,
+    info: false,
+    order: []
+}
